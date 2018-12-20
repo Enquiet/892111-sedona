9ad7e276b34bcffe82ac278b5e889b4ebe4b8bdd
Enquiet <44980432+Enquiet@users.noreply.github.com>
2018-12-20 18:51:36 +0300

    js

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,8 @@
+"use strict";
 var link = document.querySelector(".main-search-sedona");
 var popup = document.querySelector(".main-form-close");
 var arrivalDate = popup.querySelector("[name=arrival-date]");
-var dateDeparture = popup.querySelector("[name=date-of-departure]");
+
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
