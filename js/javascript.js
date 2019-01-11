e043648cad6d198e0198531b363ea381b11d5d15
Enquiet <44980432+Enquiet@users.noreply.github.com>
2019-01-11 14:00:08 +0300

    2 попытка защиты седоны

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,24 @@
 "use strict";
 var link = document.querySelector(".main-search-sedona");
-var popup = document.querySelector(".main-form-close");
+var popup = document.querySelector(".main-form");
 var submit = document.querySelector("form");
+var none = popup.querySelector(".main-form-block");
+var close = popup.querySelector(".main-form-close");
 var arrivalDate = popup.querySelector("[name=arrival-date]");
 var dateDeparture = popup.querySelector("[name=date-of-departure]");
 
 
+popup.classList.add("main-form-block");
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.toggle("main-form-link");
+    popup.classList.toggle("main-form-block");
     popup.classList.remove("modal-error");
     arrivalDate.focus();
-
 });
 submit.addEventListener("submit", function (evt) {
     if (!arrivalDate.value || !dateDeparture.value) {
         evt.preventDefault();
+        popup.classList.remove("modal-error");
         popup.classList.add("modal-error");
 
     }
@@ -23,8 +26,8 @@ submit.addEventListener("submit", function (evt) {
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
         evt.preventDefault();
-        if (popup.classList.contains("main-form-link")) {
-            popup.classList.remove("main-form-link");
+        if (popup.classList.add("main-form-block")) {
+            popup.classList.add("main-form-block");
         }
     }
 });
